c9fafe94b75fc11c39388e79d0c5006464bd7035
subaru <su-ts11@outlook.com>
2019-12-19 14:01:15 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Jisyuu
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    config.generators do |g|
+      g.test_framework  nil 
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end
+
+    config.i18n.default_locale = :ja
+
+    config.time_zone = 'Tokyo'
+    
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end

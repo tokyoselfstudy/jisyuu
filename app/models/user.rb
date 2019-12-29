21f384d7854c922cdd3594bebe9c3fb3608b2cc2
subaru <su-ts11@outlook.com>
2019-12-29 14:49:41 +0900

    create user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_one_attached :avatar
+
+  def view_full_name
+    return "#{self.family_name} #{self.first_name}"
+  end
 end

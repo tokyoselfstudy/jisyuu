6cc63db7d9cf8adeb0fdab8e1214e9e62d15cede
subaru <su-ts11@outlook.com>
2020-01-12 10:23:08 +0900

    Create i18n function

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   validates :title, presence: true
+  validates :num_of_applicant, presence: true
 
   has_one_attached :image
   belongs_to :user
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Jisyuu
     end
 
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
 
     config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local

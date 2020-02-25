f85c25feedf903ea3109b1dc038cccb995abf92a
subaru <su-ts11@outlook.com>
2020-02-25 15:32:05 +0900

    Create event-copy-func

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,13 @@ class UsersController < ApplicationController
     @future_events = Event.where(user_id: params[:id], is_deleted: false).where("event_date > ?", Time.zone.now)
     @past_events = Event.where(user_id: params[:id], is_deleted: false).where("event_date < ?", Time.zone.now)
   end
+
+  def copy_events
+    @events = Event
+                .where(user_id: params[:id], is_deleted: false)
+                .order(event_date: :desc)
+                .limit(40)
+                .page(params[:page])
+                .per(10)
+  end
 end

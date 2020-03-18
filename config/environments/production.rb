a9396f10e7088018ec8459d4f8818dad1b010c25
subaru <su-ts11@outlook.com>
2020-03-19 00:17:26 +0900

    Fix production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "jisyuu_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'tokyoselfstudy.com' }
+  config.action_mailer.default_url_options = { host: 'https://www.tokyoselfstudy.com' }
   config.action_mailer.delivery_method = :ses
 
   # Ignore bad email addresses and do not raise email delivery errors.
